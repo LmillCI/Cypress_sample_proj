c79a17e0dd9478540b3b259d973c4f23ae9dfa3b
LmillCI <168177275+LmillCI@users.noreply.github.com>
2024-07-22 16:31:26 +0530

    Update e2e.ts

--- e2e.ts
+++ e2e.ts
@@ -8,6 +8,10 @@
 // You can change the location of this file or turn off
 // automatically serving support files with the
 // 'supportFile' configuration option.
+{
+  "supportFile": false
+}
+
 //
 // You can read more here:
 // https://on.cypress.io/configuration
